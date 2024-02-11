73a3722dfa44974337d9c9a0de6d1c33cde09951
Toukinha <gustavoluis.pixell@gmail.com>
2024-02-11 08:58:58 -0300

    feat: The Note-Card component has been created

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,8 +5,9 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    '@rocketseat/eslint-config/react'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,25 @@
+import logo from './assets/logo-nlw-expert.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
 export function App() {
   return (
-    <h1  >hellow</h1>
+    <div className="mx-auto max-w-6xl my-12 space-y-6">
+      <img src={logo} alt="logo" />
+      <form className="w-full ">
+        <input
+          type="text"
+          className="w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none"
+          placeholder="Busque em suas notas..."
+        />
+      </form>
+
+      <div className="h-px bg-slate-700" />
+      <div className="grid grid-cols-3 auto-rows-[250px] gap-6">
+        <NewNoteCard />
+        <NoteCard />
+        <NoteCard />
+      </div>
+    </div>
   )
 }
-
